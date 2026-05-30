a97012469597fe93f24dca02f88c41e736a8b045
kzmgrgn-maker <kzmgrgn@gmail.com>
2026-05-30 15:57:14 +0300

    Add src/main/java/com/rektdebug/modules/RektDebugCategory.java

---
+++ src/main/java/com/rektdebug/modules/RektDebugCategory.java
@@ -0,0 +1,7 @@
+package com.rektdebug.modules;
+
+import meteordevelopment.meteorclient.systems.modules.Category;
+
+public class RektDebugCategory {
+    public static final Category CATEGORY = new Category("RektDebug");
+}
